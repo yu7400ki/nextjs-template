102a37b5699093827732cc28530297ccca2734c1
yu7400ki <yu7400ki@gmail.com>
2022-11-04 13:54:10 +0900

    fix eslint config

--- src/components/common/Card/Card.tsx
+++ src/components/common/Card/Card.tsx
@@ -6,11 +6,9 @@ type Props = {
   description: string;
 };
 
-export const Card: React.FC<Props> = ({ href, title, description }) => {
-  return (
-    <a href={href} className={styles.card}>
-      <h2>{title} &rarr;</h2>
-      <p>{description}</p>
-    </a>
-  );
-};
+export const Card: React.FC<Props> = ({ href, title, description }) => (
+  <a href={href} className={styles.card}>
+    <h2>{title} &rarr;</h2>
+    <p>{description}</p>
+  </a>
+);
