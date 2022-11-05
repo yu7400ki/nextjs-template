2126766ed9770f067b163e24dd0b4bc55dd2cea3
yu7400ki <yu7400ki@gmail.com>
2022-11-06 03:25:25 +0900

    fix types

--- src/types/option.ts
+++ src/types/option.ts
@@ -1,14 +1,58 @@
-export type Some<T> = {
-  type: "Some";
-  value: T;
-} & {
-  unwrap: () => T;
-};
+/* eslint-disable max-classes-per-file */
+/* eslint-disable class-methods-use-this */
 
-export type None = {
-  type: "None";
-} & {
-  unwrap: () => never;
-};
+export class Some<T> {
+  readonly type = "Some";
+
+  value: T;
+
+  constructor(value: T) {
+    this.value = value;
+  }
+
+  isSome(): boolean {
+    return true;
+  }
+
+  unwrap(): T {
+    return this.value;
+  }
+
+  unwrapOr(): T {
+    return this.value;
+  }
+
+  map<U>(fn: (value: T) => U): Some<U> {
+    return new Some(fn(this.value));
+  }
+
+  mapOr<U>(defaultValue: U, fn: (value: T) => U): U {
+    return fn(this.value);
+  }
+}
+
+export class None {
+  readonly type = "None";
+
+  isSome(): boolean {
+    return false;
+  }
+
+  unwrap(): never {
+    throw new Error("called unwrap on None");
+  }
+
+  unwrapOr<T>(value: T): T {
+    return value;
+  }
+
+  map(): None {
+    return new None();
+  }
+
+  mapOr<U>(defaultValue: U): U {
+    return defaultValue;
+  }
+}
 
 export type Option<T> = Some<T> | None;
--- src/types/result.ts
+++ src/types/result.ts
@@ -1,15 +1,64 @@
-export type Ok<T> = {
-  type: "Ok";
-  value: T;
-} & {
-  unwrap: () => T;
-};
+/* eslint-disable max-classes-per-file */
+/* eslint-disable class-methods-use-this */
 
-export type Err<E extends Error> = {
-  type: "Err";
+export class Ok<T> {
+  readonly type = "Ok";
+
+  value: T;
+
+  constructor(value: T) {
+    this.value = value;
+  }
+
+  isOk(): boolean {
+    return true;
+  }
+
+  unwrap(): T {
+    return this.value;
+  }
+
+  unwrapOr(): T {
+    return this.value;
+  }
+
+  map<U>(fn: (value: T) => U): Ok<U> {
+    return new Ok(fn(this.value));
+  }
+
+  mapOr<U>(defaultValue: U, fn: (value: T) => U): U {
+    return fn(this.value);
+  }
+}
+
+export class Err<E extends Error> {
+  readonly type = "Err";
+
   error: E;
-} & {
-  unwrap: () => never;
-};
+
+  constructor(value: E) {
+    this.error = value;
+  }
+
+  isOk(): boolean {
+    return false;
+  }
+
+  unwrap(): never {
+    throw new Error("called unwrap on Err");
+  }
+
+  unwrapOr<T>(value: T): T {
+    return value;
+  }
+
+  map(): Err<E> {
+    return new Err(this.error);
+  }
+
+  mapOr<U>(defaultValue: U): U {
+    return defaultValue;
+  }
+}
 
 export type Result<T, E extends Error> = Ok<T> | Err<E>;
