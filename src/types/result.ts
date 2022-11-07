0d25db0445f3025d0294585f55bd1f218e42aa6e
yu7400ki <yu7400ki@gmail.com>
2022-11-07 12:34:10 +0900

    fix result type

--- src/types/result.ts
+++ src/types/result.ts
@@ -1,5 +1,5 @@
 export type Result<T, E extends Error = Error> = {
-  readonly type: "Ok" | "Err";
+  readonly type: typeof Ok | typeof Err;
   value?: T;
   error?: E;
   isOk(): boolean;
@@ -9,42 +9,44 @@ export type Result<T, E extends Error = Error> = {
   mapOr<U>(defaultValue: U, fn: (value: T) => U): U;
 };
 
-export const Ok = <T>(value: T): Result<T, never> => ({
-  type: "Ok",
-  value,
-  isOk(): boolean {
-    return true;
-  },
-  unwrap(): T {
-    return this.value as T;
-  },
-  unwrapOr(): T {
-    return this.value as T;
-  },
-  map<U>(fn: (value: T) => U): Result<U, never> {
-    return Ok(fn(this.value as T));
-  },
-  mapOr<U>(defaultValue: U, fn: (value: T) => U): U {
-    return fn(this.value as T);
-  },
-});
+export const Ok = <T>(value: T): Result<T, never> =>
+  ({
+    type: Ok,
+    value,
+    isOk(): boolean {
+      return true;
+    },
+    unwrap(): T {
+      return this.value as T;
+    },
+    unwrapOr(): T {
+      return this.value as T;
+    },
+    map<U>(fn: (value: T) => U): Result<U, never> {
+      return Ok(fn(this.value as T));
+    },
+    mapOr<U>(defaultValue: U, fn: (value: T) => U): U {
+      return fn(this.value as T);
+    },
+  } as const);
 
-export const Err = <E extends Error = Error>(error: E): Result<never, E> => ({
-  type: "Err",
-  error,
-  isOk(): boolean {
-    return false;
-  },
-  unwrap(): never {
-    throw new Error("called unwrap on Err");
-  },
-  unwrapOr<T>(value: T): T {
-    return value;
-  },
-  map(): Result<never, E> {
-    return Err(this.error as E);
-  },
-  mapOr<U>(defaultValue: U): U {
-    return defaultValue;
-  },
-});
+export const Err = <E extends Error = Error>(error: E): Result<never, E> =>
+  ({
+    type: Err,
+    error,
+    isOk(): boolean {
+      return false;
+    },
+    unwrap(): never {
+      throw new Error("called unwrap on Err");
+    },
+    unwrapOr<T>(value: T): T {
+      return value;
+    },
+    map(): Result<never, E> {
+      return Err(this.error as E);
+    },
+    mapOr<U>(defaultValue: U): U {
+      return defaultValue;
+    },
+  } as const);
