ea044997400589c91c0219b7002124d52e561888
yu7400ki <yu7400ki@gmail.com>
2022-11-08 13:19:01 +0900

    change result type to class base

--- src/types/result.ts
+++ src/types/result.ts
@@ -1,5 +1,8 @@
-export type Result<T, E extends Error = Error> = {
-  readonly type: typeof Ok | typeof Err;
+/* eslint-disable max-classes-per-file */
+/* eslint-disable class-methods-use-this */
+
+type ResultProperties<T, E extends Error = Error> = {
+  readonly type: "Ok" | "Err";
   value?: T;
   error?: E;
   isOk(): boolean;
@@ -9,44 +12,66 @@ export type Result<T, E extends Error = Error> = {
   mapOr<U>(defaultValue: U, fn: (value: T) => U): U;
 };
 
-export const Ok = <T>(value: T): Result<T, never> =>
-  ({
-    type: Ok,
-    value,
-    isOk(): boolean {
-      return true;
-    },
-    unwrap(): T {
-      return this.value as T;
-    },
-    unwrapOr(): T {
-      return this.value as T;
-    },
-    map<U>(fn: (value: T) => U): Result<U, never> {
-      return Ok(fn(this.value as T));
-    },
-    mapOr<U>(defaultValue: U, fn: (value: T) => U): U {
-      return fn(this.value as T);
-    },
-  } as const);
+export class Ok<T> implements ResultProperties<T, never> {
+  readonly type = "Ok";
 
-export const Err = <E extends Error = Error>(error: E): Result<never, E> =>
-  ({
-    type: Err,
-    error,
-    isOk(): boolean {
-      return false;
-    },
-    unwrap(): never {
-      throw new Error("called unwrap on Err");
-    },
-    unwrapOr<T>(value: T): T {
-      return value;
-    },
-    map(): Result<never, E> {
-      return Err(this.error as E);
-    },
-    mapOr<U>(defaultValue: U): U {
-      return defaultValue;
-    },
-  } as const);
+  value: T;
+
+  constructor(value: T) {
+    this.value = value;
+  }
+
+  isOk(): this is Ok<T> {
+    return true;
+  }
+
+  unwrap(): T {
+    return this.value as T;
+  }
+
+  unwrapOr(): T {
+    return this.value as T;
+  }
+
+  map<U>(fn: (value: T) => U): Result<U, never> {
+    return new Ok(fn(this.value as T));
+  }
+
+  mapOr<U>(defaultValue: U, fn: (value: T) => U): U {
+    return fn(this.value as T);
+  }
+}
+
+export class Err<E extends Error = Error>
+  implements ResultProperties<never, E>
+{
+  readonly type = "Err";
+
+  error: E;
+
+  constructor(error: E) {
+    this.error = error;
+  }
+
+  isOk(): this is Err<E> {
+    return false;
+  }
+
+  unwrap(): never {
+    throw new Error("called unwrap on Err");
+  }
+
+  unwrapOr<T>(value: T): T {
+    return value;
+  }
+
+  map(): Result<never, E> {
+    return new Err(this.error as E);
+  }
+
+  mapOr<U>(defaultValue: U): U {
+    return defaultValue;
+  }
+}
+
+export type Result<T, E extends Error = Error> = Ok<T> | Err<E>;
